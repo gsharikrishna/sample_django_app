d83201705dd1d4f716cff2471452514084d763e4
guna sai hari krishna <saiharikrishna320@gmail.com>
2023-12-21 20:54:43 +0530

    added views.py and finalised_model.sav

--- demo/myapp/urls.py
+++ demo/myapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("",views.home,name="home"),
+    path('result/',views.result,name="result"),
     path("about",views.aboutpage,name="aboutpage")
 ]
--- demo/myapp/views.py
+++ demo/myapp/views.py
@@ -1,7 +1,28 @@
 
 from django.shortcuts import render,HttpResponse
+import joblib
 
 def home(request):
     return render(request,"home.html")
 def aboutpage(request):
     return render(request,"about.html")
+def result(request):
+
+    cls = joblib.load('finalised_model.sav')
+
+    lis = []
+
+    lis.append(request.GET['RI'])
+    lis.append(request.GET['Na'])
+    lis.append(request.GET['Mg'])
+    lis.append(request.GET['Al'])
+    lis.append(request.GET['Si'])
+    lis.append(request.GET['K'])
+    lis.append(request.GET['Ca'])
+    lis.append(request.GET['Ba'])
+    lis.append(request.GET['Fe'])
+
+
+    ans = cls.predict([lis])
+
+    return render(request,"result.html",{'ans':ans,'lis':lis})
